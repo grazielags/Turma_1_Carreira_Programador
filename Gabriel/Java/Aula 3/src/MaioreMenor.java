e5ac4868ee05dc1a867cf83d5407cf5235639eb7
gabriel kirchner <gabriel kirchner>
2016-10-01 18:07:10 -0300

    java aulas 3 e 4

---
+++ Gabriel/Java/Aula 3/src/MaioreMenor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaioreMenor {
+	public static void main(String[] args) {
+		int n1, n2, n3;
+		System.out.print("Entre com o primeiro inteiro: ");
+		Scanner sc = new Scanner(System.in);
+		n1 = sc.nextInt();
+		System.out.print("Entre com o segundo inteiro: ");
+		n2 = sc.nextInt();
+		System.out.print("Entre com o terceiro inteiro: ");
+		n3 = sc.nextInt();
+		if (n1 > n2 && n1 > n3) {
+			System.out.println("O maior numero é: " + n1);
+		} else {
+			if (n2 > n3 && n2 > n1) {
+				System.out.println("O menor numero é: " + n2);
+			} else {
+				if (n3 > n2 && n3 > n1) {
+					System.out.println("O menor numero é: " + n3);
+				}
+			}		
+		}
+	}	
+}	
