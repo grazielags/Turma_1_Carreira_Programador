9cfd95422f676d7e5099d7909424057512208621
Fernando Caser Silva <silva.caser@gmail.com>
2016-10-15 11:08:10 -0300

    Projeto Biblioteca

--- Fernando/Projeto/ProjetoBiblioteca.java
+++ Fernando/Projeto/ProjetoBiblioteca.java
@@ -12,24 +12,27 @@ public class ProjetoBiblioteca {
 		String senha;
 		String autor;
 		String livro;
-		usuario = Integer.parseInt(JOptionPane.showInputDialog("Digite a opção desejada " +
-								"\n 1 - Cadastrar" +
-								"\n	 2 - Consultar" +
-								"\n 3 - Logar" +
-								"\n 0 - Sair"));
+		usuario = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a opção desejada " +
+															"\n 1 - Cadastrar Usuários" +
+															"\n	 2 - Consultar Livros" +
+															"\n 3 - Logar" +
+															"\n 0 - Sair",
+															"Menu Principal", JOptionPane.DEFAULT_OPTION));
+														/* essa ultima linha dá nome a caixa de dialogo que aprece p/ entrada de dados */
 		switch (usuario){
 			case 1:
-				nome = JOptionPane.showInputDialog("Nome completo");
-				end = JOptionPane.showInputDialog("Endereço");
+				nome = JOptionPane.showInputDialog(null, "Nome Completo", "Cadastramento", JOptionPane.DEFAULT_OPTION);
+				end = JOptionPane.showInputDialog(null, "Endereço", "Cadastramento", JOptionPane.DEFAULT_OPTION);
 				break;
 				
 			case 2:
-				JOptionPane.showInputDialog ("Digite o Nome ou Autor do livro");
+				livro = JOptionPane.showInputDialog (null, "Digite o nome do livro", "Consulta de Livros", JOptionPane.DEFAULT_OPTION);
+				autor = JOptionPane.showInputDialog (null, "Digite o nome do autor", "Consulta de Livros", JOptionPane.DEFAULT_OPTION);
 				break;
 				
 			case 3:	
-				login = JOptionPane.showInputDialog("Login");
-				senha = JOptionPane.showInputDialog("Senha");
+				login = JOptionPane.showInputDialog(null, "Login", "Acesso Usuáios", JOptionPane.DEFAULT_OPTION);
+				senha = JOptionPane.showInputDialog(null, "Senha", "Acesso Usuáios", JOptionPane.DEFAULT_OPTION);
 				break;
 				
 			case 0:
