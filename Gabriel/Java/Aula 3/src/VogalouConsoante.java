d075896134b318636b25ff1e586aabe4c68d2c31
Gabriel Kirchner <gk-2000@hotmail.com>
2016-10-29 18:11:54 -0200

    Teste

--- Gabriel/Java/Aula 3/src/VogalouConsoante.java
+++ Gabriel/Java/Aula 3/src/VogalouConsoante.java
@@ -1,9 +1,8 @@
-
-public class VogalouConsoante {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	import javax.swing.JOptionPane;
+	
+	public class VogalouConsoante {
+			public static void main(String[] args) {
+				char letra = JOptionPane.showInputDialog("Digite uma letra.").toUpperCase().charAt(0);
+				if letra == 'A' ||
+			}
 	}
-
-}
--- Gabriel/Java/Aula 9/src/DissolvendoeSomando.java
+++ Gabriel/Java/Aula 9/src/DissolvendoeSomando.java
@@ -3,7 +3,11 @@ import javax.swing.JOptionPane;
 public class DissolvendoeSomando {
 
 	public static void main(String[] args) {
-		int num = JOptionPane.showInputDialog(null,"Digite quatr números inteiros");
+		String numero = JOptionPane.showInputDialog("Digite um número !");
+		if (numero.contains("-")) {
+			numero = numero.substring(1, numero.lenght());
+			
+		}
 
 	}
 
