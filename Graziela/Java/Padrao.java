546dfa381b04f3a9f032eddaaed3d62afe95ed33
Graziela Göedert de Souza <grazielagoedert@gmail.com>
2016-11-05 09:09:44 -0200

    Alterações

--- Graziela/Java/Padrao.java
+++ Graziela/Java/Padrao.java
@@ -6,6 +6,7 @@
 		System.out.println(".");
 		System.out.println("O que não será escrito na tela.");
 		System.out.println("O que será escrito na tela.");
+		System.out.println("Novo comentário Graziela3.");
 		System.out.println("O que será escrito na tela.");
 	}
 }
